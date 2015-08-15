0a0c195c30feead85689467dda43132ae1824d6c
@zip <zip@keybase.io>
2015-08-14 17:41:18 -0700

    Cleaned up the example

--- dancer.js
+++ dancer.js
@@ -1,10 +1,13 @@
 'use strict';
 
 window.Dancer = (function () {
+  // Helper function because JS is full of array-like objects.
   var arr = function (a) {
     return Array.prototype.slice.call(a);
   }
 
+  // Local subcopy of the console.  Later we might hook this to appear on
+  // the page, or not spam things up in debug mode, or whatever.
   var console = {
     log: window.console.log.bind(window.console),
     warn: window.console.warn.bind(window.console),
@@ -13,9 +16,15 @@ window.Dancer = (function () {
 
   // A mapping from DOM class to element constructor
   var registry = {};
+  // A mapping from an element ID to its Component
   var elements = [];
+  // The root we're observing
   var rootNode = null;
 
+  // Component constructor.
+  // Here we do administration related to tracking: setting ID, putting it in
+  // the element registry.
+  // Everything else runs in _init().
   function Component(element) {
     elements.push(this);
     element.setAttribute("dancerId", elements.length - 1);
@@ -23,12 +32,14 @@ window.Dancer = (function () {
     this._init();
   }
 
+  // Look up a component from an element
   Component.for = function(element) {
     if (element.getAttribute("dancerId")) {
       return elements[element.getAttribute("dancerId")];
     }
   }
 
+  // Look up a prospective Constructor for an element
   Component.match = function(element) {
     if (!element || element.nodeType != Node.ELEMENT_NODE) return;
     var constructor;
@@ -42,6 +53,11 @@ window.Dancer = (function () {
     return constructor;
   }
 
+  // Add a component to the registry.
+  // Arguments are a class name to match and an object.
+  // The object is copied over to our namespace and given an
+  // appropriate prototype.
+  // User should implement `attach`, `detach`, `init`, `destroy`.
   Component.register = function(className, constructor) {
     var proto = Object.create(Component.prototype);
     for (var key in constructor) {
@@ -79,6 +95,8 @@ window.Dancer = (function () {
       console.log("Inited a", this.className);
     },
     _init: function() {
+      // This is purely for spotting when a class is removed from an object
+      // while it is detached from the DOM
       this.observer = new MutationObserver(observerFunction);
       this.observer.observe(this.element, {
         attributes: true,
@@ -102,6 +120,8 @@ window.Dancer = (function () {
     }
   };
 
+  // Fired when an element is attached.  Either look it up and send it the
+  // event or create it from whole cloth.
   function attachNode(node) {
     if (node.nodeType != Node.ELEMENT_NODE) return;
     var component = Component.for(node);
@@ -118,34 +138,40 @@ window.Dancer = (function () {
   var observerFunction = function (records) {
     arr(records).map(function (record) {
       arr(record.addedNodes).map(function (node) {
+        if (node.nodeType != Node.ELEMENT_NODE) return;
+        // Hunt for other components in the subtree and attach
         for (var className in registry) {
           var selector = "." + className;
           arr(node.querySelectorAll(selector)).map(attachNode);
         }
+        // Attach root
         attachNode(node);
       });
       arr(record.removedNodes).map(function (node) {
         if (node.nodeType != Node.ELEMENT_NODE) return;
+        // Hunt for Components in the subtree and detach
         var subtree = arr(node.querySelectorAll("[dancerId]"));
         for (var i in subtree) {
           var component = Component.for(subtree[i]);
           if (component) component._detach();
         }
+        // Detach
         var component = Component.for(node);
         if (component) component._detach();
       });
       if (record.attributeName) {
+        // Create a fake element just so we can get a classList
         var tmp = document.createElement("div");
         tmp.setAttribute(record.attributeName, record.oldValue);
         var nextClasses = record.target.classList;
         var prevClasses = tmp.classList;
-        // Process declassing
+        // Process a class being removed
         var component = Component.for(record.target);
         if (component
             && !nextClasses.contains(component.className)) {
               component._destroy();
         }
-        // Process enclassing
+        // Process a class being added
         nextClasses.forEach(function (c) {
           if (!(prevClasses.contains(c))) {
             if (registry[c]) {
@@ -159,6 +185,8 @@ window.Dancer = (function () {
       }
     });
   };
+
+  // Master observer
   var observer = new MutationObserver(observerFunction);
 
   function observe(element) {
@@ -177,6 +205,7 @@ window.Dancer = (function () {
   function disconnect(element) { return observer.disconnect(element); }
 
   return {
+    for: Component.for.bind(Component),
     observe: observe,
     disconnect: disconnect,
     Component: Component,
